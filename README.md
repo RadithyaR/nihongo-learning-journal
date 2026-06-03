f021473fa2dae1c3f97b08c06e457db97b80590a
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2026-06-03 10:39:28 +0700

    feat(setup): initialize project structure

---
+++ README.md
@@ -0,0 +1,24 @@
+=== Nihongo Learning Journal ===
+
+A personal Japanese learning tracker application built with:
+
+== Frontend
+
+- Next.js
+- TypeScript
+- Tailwind CSS
+
+== Backend
+
+- Golang
+- Gin
+- GORM
+- JWT
+
+== Database
+
+- Supabase PostgreSQL
+
+== Storage
+
+- Supabase Storage
---
+++ backend/cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default();
+	port := 8000
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "server is running",
+		})
+	})
+
+	r.Run(fmt.Sprintf(": %d", port))
+}
